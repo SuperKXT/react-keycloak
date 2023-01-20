99538fdcf1a0ab8f9e461dcaf8a42d16c0020329
SuperKXT <superkxt@outlook.com>
2023-01-20 15:48:45 +0500

    Add setup-server.sh

---
+++ setup-server.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+url=https://github.com/keycloak/keycloak/releases/download/20.0.3/keycloak-20.0.3.zip
+
+GREEN='\e[32m'
+NC='\e[0m'
+
+echo -e "\n${GREEN}Downloading KeyCloak Server...${NC}" &&
+	wget -q --show-progress "$url" -O keycloak.zip &&
+	unzip -qo keycloak.zip &&
+	find server -mindepth 1 ! -regex '^./server/data\(/.*\)?' -delete &&
+	mv -f keycloak-20.0.3/* server/ &&
+	rm -rf keycloak.zip keycloak-20.0.3 &&
+	echo -e "\n${GREEN}KeyCloak server downloaded!'\n${NC}"
